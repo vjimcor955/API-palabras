1b41f092851772a7d518341f3c5b73211892cc8b
vjimcor955 <vjimcor955@g.educaand.es>
2023-12-16 17:42:27 +0100

    ControladorJugador completo

--- src/main/java/com/example/apipalabras/controlador/ControladorJugador.java
+++ src/main/java/com/example/apipalabras/controlador/ControladorJugador.java
@@ -1,13 +1,12 @@
 package com.example.apipalabras.controlador;
 
+import com.example.apipalabras.error.JugadorNotFound;
 import com.example.apipalabras.modelos.Jugador;
 import com.example.apipalabras.repos.JugadorRepositorio;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -20,10 +19,38 @@ public class ControladorJugador {
         this.jugadorRepositorio = jugadorRepositorio;
     }
 
-    // TODO: Terminar CRUD jugadores
     @GetMapping("/jugadores")
     public List<Jugador> getAllJugadores() {
         List<Jugador> allJugadores = jugadorRepositorio.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(allJugadores).getBody();
+    }
+
+    @PostMapping("/jugador")
+    public ResponseEntity<Jugador> postEquipo(@Valid @RequestBody Jugador jugador) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(jugadorRepositorio.save(jugador));
+    }
+
+    @PutMapping("/jugador/{id}")
+    public Jugador updateEquipo(@PathVariable Long id, @Valid @RequestBody Jugador jugador) {
+        return jugadorRepositorio.findById(id)
+                .map(existingJugador -> {
+                    existingJugador.setId(jugador.getId());
+                    existingJugador.setUser(jugador.getUser());
+                    existingJugador.setScore(jugador.getScore());
+                    existingJugador.setAvatar_img(jugador.getAvatar_img());
+                    existingJugador.setTeam_id(jugador.getTeam_id());
+                    return ResponseEntity.status(HttpStatus.OK).body(jugadorRepositorio.save(existingJugador)).getBody();
+                })
+                .orElseThrow(() ->  new JugadorNotFound(id));
+    }
+
+    @DeleteMapping("/jugador/{id}")
+    public ResponseEntity<?> deleteEquipo(@PathVariable Long id) {
+        return jugadorRepositorio.findById(id)
+                .map(jugador -> {
+                    jugadorRepositorio.delete(jugador);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseThrow(() -> new JugadorNotFound(id));
     }
 }
